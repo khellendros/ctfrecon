c1c7f8c510365186b94c4b8f624bfd24a23d95eb
khellendros <private>
2019-08-13 11:35:54 -0400

    first commit

---
+++ ctfrecon.py
@@ -0,0 +1,16 @@
+#! /usr/bin/python
+
+import sys
+
+if len(sys.argv) > 1
+    squery = ' '.join(argv[1:])
+else:
+    print('What are you looking for? ')
+    squery = input()
+
+if squery == ''
+    print('Search query cannot be empty')
+    sys.exit
+
+
+
