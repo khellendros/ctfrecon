c06436f24183c962642d850233a54a7afc9c4c2d
khellendros <private>
2019-08-13 19:26:17 -0400

    added formatting and some code clean-up.

--- ctfrecon.py
+++ ctfrecon.py
@@ -1,7 +1,16 @@
 import sys, os
 
-exploitspath = '/usr/share/exploitdb/files_exploits.csv'
-#shellcodespath = '/usr/share/exploitdb/files_shellcodes.csv'
+###CSV Identifiers###
+ID = 0
+FILE = 1
+DESCRIPTION = 2
+DATE = 3
+AUTHOR = 4
+TYPE = 5
+PLATFORM = 6
+PORT = 7
+#####################
+SSPATH = '/usr/share/exploitdb/' #searchsploit root dir
 
 if len(sys.argv) > 1:
     squery = ' '.join(sys.argv[1:])
@@ -14,22 +23,21 @@ if squery == '':
     sys.exit
 
 #open, read all lines into list, and error check exploits CSV
-if os.path.isfile(exploitspath) == True:
-    exploitCSV = open(exploitspath)
+if os.path.isfile(SSPATH + 'files_exploits.csv') == True:
+    exploitCSV = open(SSPATH + 'files_exploits.csv')
     exploitlist = exploitCSV.readlines()
 else:
     print(exploitspath + ' does not exist!')
     sys.exit
 
-print('Search Query: ' + squery)
-squery = squery.lower()
+print("\n------------------------>SEARCH QUERY: " + squery)
+squery = squery.lower()  #change to lowercase for matching
 
 #parse lines
-for line in exploitlist:
+for line in exploitlist[1:]:
     line.strip('/n')
     tmp = line.split(',')
-    exploitsfilepath = '/usr/share/exploitdb/' + tmp[1]
-    #print('Trying ' + exploitsfilepath + '...')   #verbose for debugging
+    exploitsfilepath = SSPATH + tmp[FILE]
     if os.path.isfile(exploitsfilepath) == True:
         with open(exploitsfilepath, 'r') as exploitFILE:
             try:
@@ -38,9 +46,10 @@ for line in exploitlist:
                     contentLine = contentLine.lower()
                     search = contentLine.find(squery)
                     if search != -1:
-                        print('FOUND IT!: ' + tmp[1])
+                            print(f"\033[1;31;40m FILE: \033[1;32;40m {tmp[FILE]} \033[1;31;40m DESCRIPTION: \033[1;32;40m {tmp[DESCRIPTION]}")
+                            break #found in file, break to prevent duplicate listing
             except:
-                print('Error opening: ' + exploitsfilepath)
+                print('Some sort of error occured: ',  sys.exc_info()[0])
 
         #reset and close file
         exploitFILEcontent.clear
@@ -49,6 +58,7 @@ for line in exploitlist:
         print('Skipping ' + exploitsfilepath + '  Does not exist!')
 
 
+print("")
 exploitCSV.close
 
 
