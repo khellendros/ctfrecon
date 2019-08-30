28847524fb6a961967e113a11310409da8f00bc0
khellendros <krhproductions@gmail.com>
2019-08-30 14:24:24 -0400

    added some error handling.  started adding code for index search functionality

--- ctfrecon.py
+++ ctfrecon.py
@@ -14,8 +14,8 @@ PORT = 7
 #-----------------------------------------
 SSPATH = '/usr/share/exploitdb/' #searchsploit root dir
 #------------------OPTIONS----------------
-unixOptions = "d:s:h"
-gnuOptions = ["deep=", "surface=", "help"]
+unixOptions = "d:i:h"
+gnuOptions = ["deep=", "index=", "help"]
 
 
 
@@ -30,32 +30,34 @@ def openCSV(PATH):   #open, read all lines from CSV index
         sys.exit()
     
 
-def deepsearch(searchStr, csvIndex):   #traverse CSV index and search all files referenced for squery
-    indexResults = []
+def searchExploits(searchStr, csvIndex, searchType):   #traverse CSV index and search all files referenced for squery
+    indexResults = [] 
 
-    print("\nWe're Going Deep!------------------------>SEARCH QUERY: " + searchStr)
+    if(searchType is "d"):
+        print("\nWe're Going Deep!------------------------>SEARCH QUERY: " + searchStr)
     
-    for line in csvIndex[1:]:                                           #loop through each line in the index 
-        line.strip('/n')           
-        tmp = line.split(',')                                           #pull each value into a list
-        exploitsFilePath = SSPATH + tmp[FILE]                           #set to current lines file value 
-        if os.path.isfile(exploitsFilePath) == True:                    #make sure file exists, skip if it doesn't
-            with open(exploitsFilePath, 'r') as exploitFILE:
-                try:
-                    exploitFileContent = exploitFILE.readlines()
-                    for contentLine in exploitFileContent:              #SEearch through file one line at a time - TODO: also search CSV index 
-                        contentLine = contentLine.lower()
-                        search = contentLine.find(searchStr)
-                        if search != -1:
-                            indexResults.append(tmp)
-                            break       #break to prevent duplicate results
-                except:
-                    print('Some sort of error occured: ',  sys.exc_info()[0])  #TODO: Do proper error handling here
+        for line in csvIndex[1:]:                                           #loop through each line in the index 
+            line.strip('/n')           
+            tmp = line.split(',')                                           #pull each value into a list
+            exploitsFilePath = SSPATH + tmp[FILE]                           #set to current lines file value 
+            if os.path.isfile(exploitsFilePath) == True:                    #make sure file exists, skip if it doesn't
+                with open(exploitsFilePath, 'r') as exploitFILE:
+                    try:
+                        exploitFileContent = exploitFILE.readlines()
+                        for contentLine in exploitFileContent:               
+                            contentLine = contentLine.lower()
+                            search = contentLine.find(searchStr)
+                            if search != -1:
+                                indexResults.append(tmp)
+                                break       #break to prevent duplicate results
+                    except:
+                        print('Some sort of error occured: ',  sys.exc_info()[0])  #TODO: Do proper error handling here
 
-                    exploitFileContent.clear
-                    exploitFILE.close
-        else:
-            print('Skipping ' + exploitsfilepath + '  Does not exist!')
+                        exploitFileContent.clear
+                        exploitFILE.close
+            else:
+                print('Skipping ' + exploitsfilepath + '  Does not exist!')
+
     return indexResults
 
 
@@ -79,7 +81,7 @@ def displayResults(rList):
 
         if select == 0:
             return 0
-        elif select < x and select > -1:
+        elif select < x and select > -1:   #TODO: Need to fix error handling for other keyboard inputs besides integers.. like arrows and shit
             if (os.path.isfile(SSPATH + rList[select - 1][FILE])) == True:
                 with open(SSPATH + rList[select - 1][FILE], 'r') as exploitFILE:
                     textFile = exploitFILE.read()
@@ -90,7 +92,10 @@ def displayResults(rList):
 
         x = 1
             
-
+def checkLen(someString):
+    if len(someString) < 4:
+        print("Search string too short!")
+        exit(0)
 
 if __name__ == "__main__":
 
@@ -107,10 +112,19 @@ if __name__ == "__main__":
         exit(0)
 
     for currentArg, currentValue in args:
-        if currentArg in ("-d", "--deep"):  #TODO:errorcheck currentValue to make sure search string is not too short resulting in too much output
-            results = deepsearch(currentValue.lower(), openCSV(SSPATH))
-            if results is not None:  #this is hilarious
-                displayResults(results)
-            else:
-                print("No results found for: " + currentValue)
-            
+        if currentArg in ("-d", "--deep"):
+            checkLen(currentValue)
+            results = searchExploits(currentValue.lower(), openCSV(SSPATH), "d")
+        elif currentArg in ("-i", "--index"):
+            checkLen(currentValue)
+            results = searchExploits(currentValue.lower(), openCSV(SSPATH), "i")
+
+        if len(results) > 0: #this is hilarious
+            displayResults(results)
+        else:
+            print("No results found for: " + currentValue)
+
+
+	
+
+
