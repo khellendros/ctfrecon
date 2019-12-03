1b4b6a5a313eb3d1e5948ecef2c412e7f8de9e27
khellendros <krhproductions@gmail.com>
2019-12-03 18:54:15 -0500

    started building out show command.  moved initial command parsing to CommandLineInterface class

--- interface/commandhandler.py
+++ interface/commandhandler.py
@@ -1,7 +1,6 @@
 import database.dbhandler as dbhandler
 
-def change_namespace(cli):
-
+def use_cmd(cli):
     newnamespace = 'project'
     cli.change_namespace(newnamespace)
     print('change namespace')
@@ -9,47 +8,61 @@ def change_namespace(cli):
 
 
 def show_cmd(cli):
+    if len(cli.cmd_breakout) < 2:
+        cli.display_msg('Invalid number of arguments to "show" --try "help"')
+        return True
 
-    print('show command')
+    if cli.cmd_breakout[1] == 'projects':
+        cli.display_msg('show projects')
+        return True
+    elif cli.cmd_breakout[1] == 'tools':
+        cli.display_msg('show tools')
+        return True
+    elif cli.cmd_breakout[1] == 'toolchains':
+        cli.display_msg('show toolchains')
+        return True
+    
+    if cli.namespace == 'project':
+        
+        if cli.cmd_breakout[1] == 'domains':
+            cli.display_msg('show domains')
+            return True
+        else:
+            cli.display_msg('Unknown option for show in %s namespace' % cli.namespace)
+            return True
+
+    elif cli.namespace == 'domain':
+
+        # validate and display enumeration attribute here
+        if cli.cmd_breakout[1] == 'enum':
+            cli.display_msg('show enum')
+            return True
+
+
     return True
 
 
 def add_cmd(cli):
-
     return True;
 
 
 def set_cmd(cli):
-
     return True;
 
 
 def run_cmd(cli):
-
     return True;
 
 
 def exit_ctfrecon(cli):
-
+    cli.display_msg('Goodbye!')
     return False;
 
 
 def no_cmd(cli):
+    cli.display_msg('Unknown Command!')
+    return True 
 
-    return 'Unkown Command'
-
-
-
-commandlist = { 'show': show_cmd, 'use': change_namespace, 'add': add_cmd, 
-                'set': set_cmd, 'run': run_cmd, 'exit': exit_ctfrecon, 
-                'quit': exit_ctfrecon}
-
-def exec_command(cli):
-   
-    cmd_breakout = cli.command.split(' ')
-    status = commandlist.get(cmd_breakout[0], no_cmd)(cli)
-
-    return status 
 
 
 """ 
--- interface/interactive.py
+++ interface/interactive.py
@@ -1,4 +1,4 @@
-from interface.commandhandler import exec_command 
+import interface.commandhandler as cmdhandler
 
 
 class CommandLineInterface():
@@ -7,25 +7,51 @@ class CommandLineInterface():
         self.command = ''
         self.namespace = 'global'
         self.prompt = 'ctfrecon# '
+        self.commandlist = {'show': cmdhandler.show_cmd, 
+                            'use': cmdhandler.use_cmd,
+                            'add': cmdhandler.add_cmd, 
+                            'set': cmdhandler.set_cmd, 
+                            'run': cmdhandler.run_cmd, 
+                            'exit': cmdhandler.exit_ctfrecon, 
+                            'quit': cmdhandler.exit_ctfrecon,
+                            'help': self.display_help} 
 
     def change_namespace(self, namespace):
         self.namespace = namespace
         self.prompt = namespace + '# '
 
     def get_cmd(self):
+        self.command = ''
+        self.cmd_breakout = []
         self.command = input('%s ' % self.prompt)
+        self.command = self.command.lower()
+        self.cmd_breakout = self.command.split(' ')
     
     def exec_cmd(self):
-        cmdstatus = exec_command(self)
-        return cmdstatus 
+        cmdstatus = self.commandlist.get(self.cmd_breakout[0], cmdhandler.no_cmd)(self)
+        return cmdstatus
 
+    def display_msg(self, message):
+        print('--- {} ---'.format(message))
+
+    def display_help(self, throwaway):
+        if self.namespace is 'global':
+            print('\nGlobal Namespace Commands:\n'
+                  '--------------------------\n'
+                  'show projects\n'
+                  'use project [project]\n'
+                  'add project [project]\n'
+                  'show tools\n'
+                  'add tool\n'
+                  'show toolchains\n'
+                  'add toolchain\n'
+                 )
+        return True
+                    
 
 interface = CommandLineInterface()
 status = True
 
 while status:
-
     interface.get_cmd()
     status = interface.exec_cmd()
-
-print("Goodbye!")
